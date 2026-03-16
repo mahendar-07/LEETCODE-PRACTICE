3ab3ac495af4eb1004bd388f408fd9828bf06ecd
mahendar-07 <mahendar10019@gmail.com>
2026-03-16 23:00:19 +0530

    leetcode 75 update

--- Blind 75/Prdofarrayexcetself.java
+++ Blind 75/Prdofarrayexcetself.java
@@ -32,8 +32,8 @@ public static void prdofarr(int nums[]){
 int m=nums.length;
 int res[]=new int[m];
 /*
-//using bruteforce method
-//it it takes more time and exceeds the time so we need the o(n) not O(n^2)
+using bruteforce method
+it it takes more time and exceeds the time so we need the o(n) not O(n^2)
 for(int i=0;i<m;i++){
 int var=1;
 for(int j=0;j<m;j++){
@@ -47,6 +47,23 @@ for(int i=0;i<m;i++){
 System.out.print(res[i]+ " ");
 }
 */
+//trying with the O(n)
+//c alculating the leftproducts
+int left[]=new int[m];
+left[0]=1;
+for(int i=1;i<m;i++){
+//using the leftproduct using prefix
+left[i]=left[i-1]*nums[i-1]; 
+}
+int rightprd=1;
+for(int i=m-1;i>=0;i--){
+res[i]=left[i]*rightprd;
+rightprd*=nums[i];
+}
+System.out.println("the resultant prdoucts except itself is :");
+for(int i=0;i<m;i++){
+System.out.print(res[i]+" ");
+} 
 }
 public static void main(String args[]){
 Scanner sc=new Scanner(System.in);
@@ -57,6 +74,12 @@ System.out.print("enter array elements:");
 for(int i=0;i<n;i++){
 nums[i]=sc.nextInt();
 }
-prdofarr(nums);
+//
+for(int i=0;i<n;i++){
+System.out.print(nums[i]+" ");
 }
+prdofarr(nums);
+sc.close();
+}
+
 }
